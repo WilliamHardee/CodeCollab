aee398f1b08e1a8f99d6be640d580473c3f7b660
William Hardee <williamhardee6@gmail.com>
2024-07-09 11:25:33 -0400

    inital commit

---
+++ backend/src/main/java/com/collabcode/collabcode/CollabcodeApplication.java
@@ -0,0 +1,13 @@
+package com.collabcode.collabcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabcodeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/collabcode/collabcode/CollabcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabcode.collabcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
