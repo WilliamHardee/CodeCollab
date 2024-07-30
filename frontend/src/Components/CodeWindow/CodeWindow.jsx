c6d6799854782e61fde18851660941a0d836be37
William Hardee <williamhardee6@gmail.com>
2024-07-30 13:39:37 -0400

    replaced react codemirror with the normal one, easier to use with yjs

--- frontend/src/Components/CodeWindow/CodeWindow.jsx
+++ frontend/src/Components/CodeWindow/CodeWindow.jsx
@@ -1,68 +1,56 @@
 import React, { useEffect, useRef, useState } from "react";
 import style from "../../Styles/codewindow.module.css";
 import { useParams } from "react-router-dom";
-import Editor from "./Editor";
 import { Stomp, Client } from "@stomp/stompjs";
 import SockJs from "sockjs-client";
 import session from "../../Session";
 import CodeNavbar from "./CodeNavbar";
 import { WebContainer } from "@webcontainer/api";
-
-
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+import { javascript } from "@codemirror/lang-javascript";
+import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
+import {EditorState} from "@codemirror/state"
+import {EditorView, keymap} from "@codemirror/view"
+import {defaultKeymap} from "@codemirror/commands"
+import {basicSetup} from 'codemirror'
+import {coolGlow} from 'thememirror'
 
 function CodeWindow() {
   const { id } = useParams();
   const [stompClient, setStompClient] = useState();
-  const [updates, setUpdates] = useState("");
+  const [code, setCode] = useState("");
   const [connected, setConnect] = useState(false);
   const [project, setProject] = useState(null);
-  const [webContainerInstance, setWebContainerInstance] = useState(null)
+  const [webContainerInstance, setWebContainerInstance] = useState(null);
   const initializationAttempted = useRef(false);
-  const iframeRef = useRef(null)
-
+  const iframeRef = useRef(null);
+  const codeMirrorRef = useRef(null)
 
   async function run() {
     const files = {
-      'main.py': {
+      "main.py": {
         file: {
-          contents: updates
-        }
-      }
-    }
+          contents: code,
+        },
+      },
+    };
 
-    await webContainerInstance.mount(files)
-    const main = await webContainerInstance.fs.readFile('main.py', 'utf-8');
+    await webContainerInstance.mount(files);
+    const main = await webContainerInstance.fs.readFile("main.py", "utf-8");
     console.log(main);
-    await webContainerInstance.spawn('apt-get', ['install', 'python3.6'])
-    const process = await webContainerInstance.spawn('python3', ['main.py']);
+    await webContainerInstance.spawn("apt-get", ["install", "python3.6"]);
+    const process = await webContainerInstance.spawn("python3", ["main.py"]);
 
-    let result = ""
-    process.output.pipeTo(new WritableStream({
-      write(data) {
-        result += data
-        iframeRef.current.contentWindow.document.body.innerHTML = result
-      }
-    }))
-
-
-  }
-
-  function connect() {
-    const socket = new SockJs("http://localhost:8080/connectToProject");
-    const tempstompClient = Stomp.over(socket);
-    tempstompClient.debug = function () {};
-    tempstompClient.connect({}, function (frame) {
-      setConnect(true);
-      setStompClient(tempstompClient);
-      console.log("Connected");
-      tempstompClient.subscribe(`/topic/project/${id}`, (message) => {
-        const res = JSON.parse(message.body);
-        if (res.from !== session.getSession("username")) {
-          console.log(res);
-          setUpdates(res.content);
-        }
-      });
-    });
+    let result = "";
+    process.output.pipeTo(
+      new WritableStream({
+        write(data) {
+          result += data;
+          iframeRef.current.contentWindow.document.body.innerHTML = result;
+        },
+      })
+    );
   }
 
   function saveProject() {
@@ -74,11 +62,10 @@ function CodeWindow() {
       },
       body: JSON.stringify({
         projectId: id,
-        projectData: updates,
+        projectData: code,
       }),
     })
       .then((response) => {
-        console.log("Response status:", response.status);
         if (!response.ok) {
           return response.text().then((text) => {
             throw new Error(
@@ -98,7 +85,7 @@ function CodeWindow() {
       .then((res) => {
         if (res.status == 200) {
           setProject(res.project);
-          setUpdates(res.project.projectData);
+          setCode(res.project.projectData);
         }
       })
       .catch((err) => {
@@ -106,27 +93,33 @@ function CodeWindow() {
       });
   }
 
-  function sendMessage() {
-    stompClient.send(
-      `/chat/connect/${id}`,
-      {},
-      JSON.stringify({ from: session.getSession("username"), content: updates })
-    );
-  }
 
   useEffect(() => {
     if (project) {
       saveProject();
     }
-  }, [updates]);
+  }, [code]);
 
+  useEffect(() => {
+    let startState = EditorState.create({
+      doc: code,
+      extensions: [basicSetup, coolGlow],
+    })
+
+    let view = new EditorView({
+      state: startState,
+      parent: document.querySelector("#editorContainer")
+    })
+
+    return() => view.destroy()
+  })
   useEffect(() => {
     async function bootWebContainer() {
       if (!initializationAttempted.current) {
         initializationAttempted.current = true;
         try {
           const instance = await WebContainer.boot();
-          setWebContainerInstance(instance)
+          setWebContainerInstance(instance);
         } catch (error) {
           console.error("Error booting WebContainer", error);
         }
@@ -134,23 +127,21 @@ function CodeWindow() {
     }
 
     if (iframeRef.current) {
-      iframeRef.current.srcdoc = `<html><body style=${"color:white; font-size: 2rem;"}></body></html>`;
+      iframeRef.current.srcdoc = `<html><body style=${"color:white; font-size: 2rem;"}><code></code></body></html>`;
     }
 
     getProjectDetails();
     bootWebContainer();
   }, []);
 
-  function onChange(value) {
-    setUpdates(value);
-  }
+  
+
 
   return (
     <>
-      <CodeNavbar onRun={run}/>
+      <CodeNavbar onRun={run} />
       <div className={style.ide}>
-        <div className={style.editorContainer}>
-          <Editor val={updates} onChange={onChange} />
+        <div id="editorContainer" className={style.editorContainer}>
         </div>
 
         <iframe ref={iframeRef} titel="output"></iframe>
